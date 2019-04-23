7fd86cbe9b9a5523301e156ba13bf4e2d4351bc0
anyingying <1107221464@qq.com>
2019-04-23 17:48:53 +0800

    add ayy_hanshu.py

---
+++ ayy_hanshu.py
@@ -0,0 +1,52 @@
+# #------------------------------
+# a,b=0,1
+# while b<10:
+#     print(b)
+#     a,b=b,a+b
+# print('end')
+
+# #-----------------------
+# # ayy=[1,2,3,4,6]
+# # print (ayy)
+# #-------------------------------
+# x=int(input("请输入整数：") )
+# if x<0:
+#     x=0
+#     print('默认x为0；')
+# elif x==0:
+#     print("x正好为0")
+# elif x>0:
+#     print(x)
+
+# #-------------------------------------
+# words =['cat','dog','tigers']
+# for w in words:
+#     print(w,len(w))
+
+# #-------------------------------------
+# words =['beauty','big','very_beauty3']
+# for w in words[:]:
+#     if len(w)>10:
+#         words.insert(0,w)
+#         print(words)
+# #-------------------------------------
+# for i in range(5,100,10):
+#     print(i)
+# a = ['mary','lucy','dingding','lala','0']
+# for w in range (len(a)):
+#     print (w,a[w])
+# list(range(5))
+# #----------------------------------------------
+# #4.6.定义函数
+# def fib(n):
+#     # print（“print a fibonacci sreies uo to n”,n）
+#     """print a fibonacci series up to n."""
+#     a,b = 0,1
+#     while a < n:
+#         print(a,end = '')
+#         a,b =b,a+b
+#     print()
+
+
+
+
