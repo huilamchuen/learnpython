1fe9e56020618234b4d1fcfcb5f2d1339ac83868
anyingying <1107221464@qq.com>
2019-04-24 20:31:33 +0800

    add for_loop

---
+++ ayy_for_loop.py
@@ -0,0 +1,28 @@
+# 任务三（for循环）
+# 1、计算1+2+3+4……+100的结果
+# 2、遍历字符串，纵向打印“不要说我不能”
+
+# 1、计算1+2+3+4……+100的结果
+
+# sum = 0
+# for i in range (1,101):
+#     # sum = sum + i
+#     sum += i
+#     print("当前和为：%s"%sum)
+# print(sum)
+
+
+# 2、遍历字符串，纵向打印“不要说我不能”
+string = '不要说我不能'
+# print(string)
+#****************************8
+# print(string[0])
+# print(string[1])
+# print(string[2])
+# print(string[3])
+# print(string[4])
+# print(string[5])
+# ********************************
+# 纵向打印
+for s in string:
+    print(s)
