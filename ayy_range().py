7cbffa97f4ce0bef3f97aebddab906bc94077837
anyingying <1107221464@qq.com>
2019-05-06 20:57:35 +0800

    add range &while

---
+++ ayy_range().py
@@ -0,0 +1,12 @@
+# range()函数用法详解
+for i in range(3,6):
+    print(i,end = " ")
+    # print(i)
+print(end=" \n")
+
+# 打印九九乘法表
+
+# for i in range(1,10):
+#     for m in range(1,i+1):
+#         print("%d * %d = %d  "%(m ,i,i*m),end= " ")
+#     print()
---
+++ ayy_while&for.py
@@ -0,0 +1,1 @@
+# 简单举例while / for 与break  continue  pass的组合用法
